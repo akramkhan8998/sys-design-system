029049dc40ba48450460c676fc3fcaca0442555c
ayushman76 <avenuedesign768@gmail.com>
2022-11-27 08:57:51 +0530

    Bugfix-1

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+// collapse sidebar 
 let sideBar = document.querySelector('.sys-sidebar-menu');
 collapseSidebar.addEventListener('click', function(){
     sideBar.classList.toggle('sidebar-collapse');
 })
+
+
+
+function infofunc(e, info) {
+    var i, content, links;
+    content = document.getElementsByClassName("sys-sub-menu");
+    for (i = 0; i < content.length; i++) {
+       content[i].style.display = "none";
+    }
+    links = document.getElementsByClassName(" sysdropDown");
+    for (i = 0; i < links.length; i++) {
+    }
+    document.getElementById(info).style.display = "block";
+   
+ }
